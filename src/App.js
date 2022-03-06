eb1d48cffe21c5e44db00d4dc32790e1c227fa46
Ekta Kumari <70444139+ektaa13jan94@users.noreply.github.com>
2022-03-06 21:17:12 +0530

    Implementation for : You will be asked to recreate the UI below. When the mouse is clicked and dragged, any intersecting boxes will be highlighted blue. On mouse up, the selection highlight disappears.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  let [selectedRows, setSelectedRows] = useState({
+    1: false,
+    2: false,
+    3: false,
+    4: false,
+    5: false,
+    6: false,
+  });
+
+  let [selectedColumns, setSelectedColumns] = useState({
+    1: false,
+    2: false,
+    3: false,
+    4: false,
+    5: false,
+    6: false,
+  });
+
+  let [startingRow, setStartingRow] = useState(undefined);
+  let [startingColumn, setStartingColumn] = useState(undefined);
+
+  let [lastSelectedRow, setLastSelectedRow] = useState(undefined);
+  let [lastSelectedColumn, setLastSelectedColumn] = useState(undefined);
+
+  let onDragStart = (row, column) => {
+    setStartingRow(row);
+    setStartingColumn(column);
+
+    setLastSelectedRow(row);
+    setLastSelectedColumn(column);
+
+    selectedRows[row] = true;
+    selectedColumns[column] = true;
+    setSelectedRows({ ...selectedRows });
+    setSelectedColumns({ ...selectedColumns });
+  };
+
+  let onDragEnter = (row, column) => {
+    if (column >= startingColumn) {
+      for (let colIter = lastSelectedColumn; colIter > column; colIter--) {
+        selectedColumns[colIter] = false;
+      }
+    }
+
+    if (row >= startingRow) {
+      for (let rowIter = lastSelectedRow; rowIter > row; rowIter--) {
+        selectedRows[rowIter] = false;
+      }
+    }
+
+    setLastSelectedColumn(column);
+    setLastSelectedRow(row);
+
+    selectedRows[row] = true;
+    selectedColumns[column] = true;
+    setSelectedRows({ ...selectedRows });
+    setSelectedColumns({ ...selectedColumns });
+  };
+
+  let onDragEnd = () => {
+    [1, 2, 3, 4, 5, 6].forEach((i) => {
+      selectedRows[i] = false;
+      selectedColumns[i] = false;
+    });
+
+    setSelectedRows({ ...selectedRows });
+    setSelectedColumns({ ...selectedColumns });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="root">
+      {[1, 2, 3, 4, 5, 6].map((activeRow) => {
+        return (
+          <div className="row" key={`row-${activeRow}`}>
+            {[1, 2, 3, 4, 5, 6].map((activeColumn) => (
+              <div
+                className={
+                  selectedRows[activeRow] === true && selectedColumns[activeColumn] === true
+                    ? 'blue box'
+                    : 'box'
+                }
+                key={`cell-${activeRow}${activeColumn}`}
+                onDragStart={() => onDragStart(activeRow, activeColumn)}
+                onDragEnter={() => onDragEnter(activeRow, activeColumn)}
+                onDragEnd={() => onDragEnd()}
+              ></div>
+            ))}
+          </div>
+        );
+      })}
     </div>
   );
 }
